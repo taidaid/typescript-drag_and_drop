626ff6e3546ba01eb916c984a1d97f201bd3f469
Bryan Windsor <bryan.windsor@ordina.nl>
2020-02-14 23:48:51 +0100

    added lodash with typescript declaration file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import lodash from "lodash";
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
+
+console.log(lodash.shuffle([3, 2, 1]));
